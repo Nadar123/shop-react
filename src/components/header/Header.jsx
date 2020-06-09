228d2712d8265ec7e7717d62e6f56a09ddc4193a
nadar123 <nadar.rosano@gmail.com>
2020-06-09 20:14:06 +0300

    redux

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './header.scss';
 import {auth} from '../../firebase/firebase.utils';
 
@@ -25,4 +26,9 @@ const Header = ({currentUser}) => {
   )
 }
 
-export default Header;
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
